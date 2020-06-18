7e69fc375e304fa6b564ca4e07604525717b7db4
kashy <kashy@sfu.ca>
2020-06-17 20:01:53 -0700

    IndexAt() Method added and code refactorin

--- Array/src/com/SixLink/Array.java
+++ Array/src/com/SixLink/Array.java
@@ -6,7 +6,9 @@ public class Array {
     private int[] array;
     private int[] copyArray;
     private int index = 0;
+    private int count;
     private int length;
+    private int indexOf;
 
     public Array(int length){
         setArray(length);
@@ -24,41 +26,67 @@ public class Array {
     }
 
     public void insert(int item){
-        if (index < length) {
-            array[index] = item;
-            index++;
-        }else {
-            length++;
-            copyArray = array;
-            setArray(length);
-            //transferArray();
-            System.arraycopy(copyArray,0,array,0,copyArray.length);
-            //array = copyArray;
-            insert(item);
+
+        if (array.length == count){
+            int[] newArray = new int[count * 2];
+            for (int i = 0; i < count; i++){
+                newArray[i] = array[i];
+            }
+            array = newArray;
         }
+
+        array[count] = item;
+        count ++;
+//        if (index < length) {
+//            array[index] = item;
+//            index++;
+//        }else {
+//            length++;
+//            copyArray = array;
+//            setArray(length);
+//            //transferArray();
+//            System.arraycopy(copyArray,0,array,0,copyArray.length);
+//            //array = copyArray;
+//            insert(item);
+//        }
     }
     public void removeAt(int index){
-        copyArray = array;
-        length--;
-        int validIndex = 0;
-        setArray(length);
-        for (int i = 0; i< copyArray.length;i++){
-            if (i == index){
-                continue;
-            }
-            array[validIndex] = copyArray[i];
-            validIndex++;
+//        copyArray = array;
+//        length--;
+//        int validIndex = 0;
+//        //setArray(length);
+//        for (int i = index; i< length;i++){
+//            if (i == index){
+//                continue;
+//            }
+//            array[validIndex] = copyArray[i];
+//            validIndex++;
+//
+//        }
+        for (int i = index; i < count-1;i++)
+            array[i] = array[i+1];
 
+        //length--;
+        count--;
+    }
+
+    public void indexAt(int target){
+
+        for (int i = 0; i < count; i++){
+            if (array[i] == target){
+                indexOf = i;
+                break;
+            } else indexOf = -1;
+        }
+        System.out.println(target + " Index is: " + indexOf);
+    }
+    public void print(){
+        //System.out.println(Arrays.toString(array));
+        for (int i = 0; i < count; i++){
+            System.out.println(array[i]);
         }
 
 
     }
-    public void print(){
-        System.out.println(Arrays.toString(array));
 
-
-    }
-    public void indexOf(){
-
-    }
 }
--- Array/src/com/SixLink/Main.java
+++ Array/src/com/SixLink/Main.java
@@ -3,13 +3,15 @@ package com.SixLink;
 public class Main {
 
     public static void main(String[] args) {
-        var array = new Array(3);
-//        array.insert(10);
-//        array.insert(20);
-//        array.insert(30);
-//        array.insert(40);
+        var array = new Array(4);
+        array.insert(10);
+        array.insert(20);
+        array.insert(30);
+        array.insert(40);
+
+
+        array.removeAt(1);
         array.print();
-//        array.removeAt(0);
-//        array.print();
+        array.indexAt(5);
     }
 }
