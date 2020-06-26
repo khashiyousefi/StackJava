cca563d2c61fdd75ff7b19a05e87f1bb9e36ec92
kashy <kashy@sfu.ca>
2020-06-26 13:42:29 -0700

    Dynamic Stack with Arrays

--- Stack/src/com/SixLink/Main.java
+++ Stack/src/com/SixLink/Main.java
@@ -7,13 +7,36 @@ public class Main {
 
     public static void main(String[] args) {
 
-        String str = "(abcd";
-        var reverse = new reverse();
-        reverse.setReversed(str);
-        var check = new theBalancedExpression();
-        String result = check.balancedExpression(str);
-        System.out.println(result);
+//        String str = "(abcd";
+//        var reverse = new reverse();
+//        reverse.setReversed(str);
+//        var check = new theBalancedExpression();
+//        String result = check.balancedExpression(str);
+//        System.out.println(result);
+        stack stack = new stack();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+//        stack.push(5);
+//        stack.push(6);
+//        stack.push(7);
+//        stack.push(8);
+//        stack.push(9);
+//        stack.push(10);
+        int value = stack.pop();
 
+        var result = stack;
+        result.print();
+//        System.out.println(value);
+//        System.out.println(stack.pop());
+//        result.print();
+//        stack.pop();
+//        result.print();
+        stack.push(5);
+//        System.out.println(stack.isEmpty());
+        stack.print();
+//        System.out.println(stack.peek());
 
     }
 
---
+++ Stack/src/com/SixLink/stack.java
@@ -0,0 +1,57 @@
+package com.SixLink;
+
+import java.util.Arrays;
+
+public class stack {
+    private int[] array = new int[5];
+    private int size = 0;
+
+    public stack(){
+
+    }
+    public int pop(){
+        if (size <= 0)
+            throw new IllegalStateException();
+        int copy= array[size-1];
+        size--;
+        newArray(size);
+        return copy;
+    }
+
+    public void push(int item){
+        if(isEmpty()){
+            newArray(5);
+        }
+        if (array.length <= size){
+            newArray(size*2);
+        }
+        array[size] = item;
+        size++;
+    }
+
+    private int[] newArray(int length){
+        var copyArray = array;
+        array = new int[length];
+        for (int i = 0; i<size;i++){
+            array[i] = copyArray[i];
+        }
+        return array;
+    }
+
+    public int peek(){
+        if (size<=0){
+            throw new IllegalStateException();
+        }
+        return array[size-1];
+    }
+    public boolean isEmpty(){
+        return (size <= 0);
+    }
+    public void print(){
+        int[] printArray = new int[size];
+        for (int i = 0;i<size;i++){
+            printArray[i] = array[i];
+        }
+        System.out.println(Arrays.toString(printArray));
+    }
+}
